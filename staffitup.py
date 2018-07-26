61c93c0aff455822108b0e63ca50ca40c4e8709e
Mila Katajisto <32310572+picada@users.noreply.github.com>
2018-07-27 01:31:47 +0300

    Initial commit

---
+++ staffitup.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/test")
+def testpage():
+    return render_template("test.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
